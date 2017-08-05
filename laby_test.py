f86ff604635ca438664dba2a7965c709106cc7bd
YANN HAMDI <hamdiyann@gmail.com>
2017-08-05 17:41:34 +0200

    creation classe labyrinthe

--- laby_test.py
+++ laby_test.py
@@ -2,23 +2,43 @@
 # -*- coding: Utf-8 -*
 from tkinter import *
 
+class Labyrinthe:
+    WIDTH_CASE= 25
+    ZONE_MUR= []
+    ZONE_LIBRE= []
+    EXIT=[]
 
+    def __init__(self, longueur, largeur):
+	    self.longueur= longueur
+	    self.largeur= largeur
+        
 
-fen = Tk()
-can = Canvas(fen, width=25*15, height=25*15, background="white")
-############################"#########" 
-ton_image = PhotoImage(file="mur.gif") 
+    def initialize_laby(self):
+        fen = Tk()
+        can = Canvas(fen, width= Labyrinthe.WIDTH_CASE * self.longueur, height= Labyrinthe.WIDTH_CASE * self.largeur, background="white")
+        ton_image = PhotoImage(file="mur.gif")   
+        j=0
+        with open("laby_test.txt", 'r') as f:# tu vas lire le fichier ligne par ligne
+            for line in f:# tu as ta ligne => line
+                for i in range(len(line)):# tu as le nombre de lettre de ta ligne len(line)
+                    if line[i] == "m":  # tu as la lettre
+                        can.create_image(i * Labyrinthe.WIDTH_CASE, j * Labyrinthe.WIDTH_CASE, anchor=NW, image= ton_image) 
+                        mur=(i, j)
+                        self.ZONE_MUR.append(mur)   
+                    elif line[i] == "0":
+                        vide=(i,j)
+                        self.ZONE_LIBRE.append(vide)
+                    elif line[i] == "E":
+                        sortie=(i,j)
+                        self.EXIT.append(sortie)
+                j += 1
+        can.pack(padx=5, pady=5)
+        fen.mainloop()
 
-longueur_case = 25
-j=0# sera le num de ligne
-with open("laby.txt", 'r') as f:# tu vas lire le fichier ligne par ligne
-   for line in f:# tu as ta ligne => line
-        for i in range(len(line)):# tu as le nombre de lettre de ta ligne len(line)
-            
-            if line[i] == "m":  # tu as la lettre
-                can.create_image(i * longueur_case, j * longueur_case, anchor=NW, image= ton_image)   
-        j += 1
+def main():
+    laby= Labyrinthe(15,15)
+    laby.initialize_laby()
 
-can.pack(padx=5, pady=5)
-fen.mainloop()
+main()
+
 

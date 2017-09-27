6f2b4d47914b0794c799edcc04a0c1f254025ee0
YANN HAMDI <hamdiyann@gmail.com>
2017-09-27 18:23:27 +0400

    rajout module dans fichier separement

---
+++ labyr.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: Utf-8 -*
+import labyrinthe
+
+import position
+
+
+def main():
+    
+    
+    labyrinthe()
+    Position()
+    labyrinthe.changing()
+
+    
+main()

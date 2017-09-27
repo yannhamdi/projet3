5506cd68caa89cf5a5e0d0e8f85ec045c32c9caa
YANN HAMDI <hamdiyann@gmail.com>
2017-09-27 18:22:34 +0400

    rajout commentaires

--- labypygame.py
+++ labypygame.py
@@ -90,16 +90,16 @@ class Labyrinthe:
         "function that places the 3 objects randomly"
         x=1 
         while x<=3:  
-            i= random.randint(0,14)   #we pick up a random number between 0 and 14
-            j= random.randint(0,14)
-            if self.laby_area[i,j]== "0":
-                self.laby_area[i,j]= str(x)
+            i= random.randint(0,14)   #we pick up a random number between 0 and 14 in order to pick up a random column
+            j= random.randint(0,14)   # random line
+            if self.laby_area[i,j]== "0":   # if the coordinate picked up is free
+                self.laby_area[i,j]= str(x) # we save our object
                 x += 1
     
     def changing(self):
         position=Position()
         while True:
-            a, b = self.mg[0]
+            a, b = self.mg[0]  # we save a and b as the actual coordinate of macgyver
             pygame.time.Clock().tick(30)
             for event in pygame.event.get():
                 # if the player presses echap we stop the game
@@ -121,12 +121,14 @@ class Labyrinthe:
                     # if the player presses the keyright
                     elif event.key == K_RIGHT:
                         new_x, new_y = (a+ 1), b
-                       
+                    # we check if the movement is inside the labyrinth   
                     if new_x in range(15) and new_y in range(15):
+                        # if it is then we call our method which test the mouvement
                         position.testing_position(self,a, b, new_x, new_y)
                     else:
                         print("sorry you are out of the layout")
-                    self.draw_laby()
+                    self.draw_laby() # we call up the method to upddate our labyrinthe
+            # if macgyver arrives at the exit then we stop the loop and test if we won or loose
             if self.mg[0]== self.victory:
                 break
         if self.ending_game()== True:
@@ -138,12 +140,15 @@ class Labyrinthe:
    
     def ending_game(self):
         "function that checks if Mac gyver has won the game or loose the game"
+        # mac gyver has arrived at the exit we check if he has the 3 objects
         if len(self.object_picked_up)==3:
             return True
+        # we check if he hasnt got the 3 objects and then he has lost
         elif len(self.object_picked_up)<= 3:
             return False
     
     def moving_gyver(self, new_x, new_y):
+        "function that gives the new coordinates of macgyver"
         self.mg[0] = new_x, new_y
              
 
@@ -151,17 +156,21 @@ class Position():
 
     def checking_coordinates(self,labyrinthe, x,y):
         "function thats checks coordinates"
+        # we return what has the coordinates provided
         return labyrinthe.laby_area[x,y] 
      
     def picking_up(self,labyrinthe, new_x, new_y):
         "function that picks up objects"
         if self.checking_coordinates(labyrinthe,new_x, new_y)== "1":
+            # we add to the list object 1
             labyrinthe.object_picked_up.append("object1")
             print("Great! You found object1")
         elif self.checking_coordinates(labyrinthe,new_x, new_y)== "2":
+            # we add to the list object 2
             labyrinthe.object_picked_up.append("object2")
             print("Great! You found object 2")
         elif self.checking_coordinates(labyrinthe,new_x, new_y)== "3":
+            # we add to the list object 3
             labyrinthe.object_picked_up.append("object3")
             print("Great! You found object 3")   
     
@@ -179,27 +188,7 @@ class Position():
             labyrinthe.draw_laby()
         
 
-    
 def main():
-    
-    
-   
-    #continuer=1
-    #while continuer:
-     #   pygame.time.Clock().tick(30)
-      #  for event in pygame.event.get():
-       #     if event.type==QUIT:
-               # continuer = 0
-        #    elif event.type== K_RIGHT:
-         #       macgyver.move_right()
-          #  elif event.type == K_LEFT:
-           #     macgyver.move_left()
-            #elif event.type== K_UP:
-             #   macgyver.move_up()
-            #elif event.type== K_DOWN:
-              #  macgyver.move_down()
-
-        #continue
     
     laby=Labyrinthe()
     laby.placing_object()
